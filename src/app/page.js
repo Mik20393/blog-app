49d36747b396d5cb669b415d04c9087fe2657285
Michael Hird <mhird@smartergridsolutions.com>
2024-06-07 15:28:08 +0100

    Adding footer and updatin styling

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,12 @@ import HeroImage from "@/components/hero-img";
 export default function Home() {
   return (
     <>
-      <main>
+      <main className={styles.home}>
         <HeroImage />
         <h1>Home Page</h1>
+      </main>
+      <main className={styles.home}>
+        <h1>About Page</h1>
       </main>
     </>
   );
--- src/components/hero-img.js
+++ src/components/hero-img.js
@@ -11,7 +11,7 @@ export default function HeroImage() {
                 src={bannerImage} 
                 alt="background image" 
                 fill={true}
-                objectFit='cover'
+                
                 />
             </div>
         </>
