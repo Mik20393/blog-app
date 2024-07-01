c9a69c51f1e14d9aacf313d54ef432c8fb1b965b
Michael Hird <mhird@smartergridsolutions.com>
2024-07-01 16:49:24 +0100

    Adding scaffold layout for about and conatact page

--- src/app/page.js
+++ src/app/page.js
@@ -10,10 +10,30 @@ export default function Home() {
         <h1>Home Page</h1>
       </section>
       <section className={styles.baseSection} id="about">
-        <h1>About Section</h1>
+        <div className={styles.title}>
+          <h1>About Section</h1>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.column}>
+            <h1>Column 1</h1>
+          </div>
+          <div className={styles.column}>
+          <h1>Column 2</h1>
+          </div>
+        </div>
       </section>
       <section className={styles.baseSection} id="contact">
-        <h1>Contact Section</h1>
+      <div className={styles.title}>
+          <h1>Contact Section</h1>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.column}>
+            <h1>Column 1</h1>
+          </div>
+          <div className={styles.column}>
+          <h1>Column 2</h1>
+          </div>
+        </div>
       </section>
     </>
   );
